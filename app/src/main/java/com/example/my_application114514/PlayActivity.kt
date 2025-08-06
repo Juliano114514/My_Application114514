2034a58ba168542fad4d9434f31e6ee91cae2b8f
Juliano114514 <1005447990@qq.com>
2025-08-06 11:06:18 +0800

    电脑在自己提交代码

---
+++ app/src/main/java/com/example/my_application114514/PlayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.my_application114514
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.play_layout)
+    }
+}
