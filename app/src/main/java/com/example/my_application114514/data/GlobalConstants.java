7ff864e5ee2f9363f374d791883e7b3fbfa66f94
Juliano114514 <1005447990@qq.com>
2025-08-06 10:34:39 +0800

    kotlin虫豸版

--- app/src/main/java/com/example/my_application114514/data/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.my_application114514.data;
-
-public class GlobalConstants {
-  public static final String KEY_SONG_LIST = "key_song_list";
-  public static final String KEY_SONG_INDEX = "key_song_index";
-  public static final String KEY_SONG_BINDER = "key_song_binder";
-  public static final String KEY_SONG_PLAY_MODE = "key_song_play_mode";
-
-}
